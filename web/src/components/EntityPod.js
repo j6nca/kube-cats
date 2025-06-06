d2c9e290853b8c54526a856d5c9a5c93f18459dd
jonathan ng <me@j6n.ca>
2025-06-05 23:25:44 -0400

    add more sprites and randomization

--- web/src/components/EntityPod.js
+++ web/src/components/EntityPod.js
@@ -1,8 +1,19 @@
-import cat from '../assets/pods/cat.gif'
+import cat_1 from '../assets/pods/cat_1.gif'
+import cat_2 from '../assets/pods/cat_2.gif'
+import cat_3 from '../assets/pods/cat_3.gif'
+import cat_4 from '../assets/pods/cat_4.gif'
+import cat_5 from '../assets/pods/cat_5.gif'
 
+const cats = [
+  cat_1,
+  cat_2,
+  cat_3,
+  cat_4,
+  cat_5,
+]
 function EntityPod({pod, podIndex}) {
   return <div className="pod" style={{ zIndex: 0/*zIndex: `${nodeIndex * 2 + (podIndex % 3 > 0 ? 1 : -1)}` , top: `${podIndex/5}%`, left: `${35 + (podIndex % 2 > 0 ? 1 : -1) * Math.random() * (30 + 5) - 5}%`*/}}>
-    <img title={pod.name} className="pod" /*style={{ transform: `scaleX(${podIndex % 2 > 0 ? 1 : -1})` }}*/ src={cat}/>
+    <img title={pod.name} className="pod" /*style={{ transform: `scaleX(${podIndex % 2 > 0 ? 1 : -1})` }}*/ src={cats[podIndex % cats.length]}/>
     <span className="label">{pod.name}</span>
   </div>;
 }
--- web/src/config.js
+++ web/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // api_url: process.env.API_URL || 'http://localhost:8001',
-    api_url: process.env.API_URL || 'https://kube-cats-api.j6n.dev',
+    api_url: process.env.API_URL || 'http://localhost:8001',
+    // api_url: process.env.API_URL || 'https://kube-cats-api.j6n.dev',
     environment: process.env.NODE_ENV || 'development',
     max_pods: process.env.MAX_PODS_PER_NODE || 200,
     show_labels: process.env.SHOW_LABELS || true,
